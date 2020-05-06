d01124a3c375847fc0a238554ff06e610c3ac6cd
Corban Raun <corban@raunco.co>
2020-05-06 11:16:24 -0600

    Fix tests

--- pkgs/test/default.nix
+++ pkgs/test/default.nix
@@ -1,13 +1,23 @@
 { pkgs, callPackage }:
 
 with pkgs;
+let
+  isDarwin = pkgs.system == "x86_64-darwin";
 
-{
-  graalvm11-ce-bin = callPackage ../development/compilers/graalvm/test-bin.nix { javaVersion = "11"; };
-  graalvm8-ce-bin = callPackage ../development/compilers/graalvm/test-bin.nix { javaVersion = "8"; };
-  hello = callPackage ./hello/test.nix {};
-  nodejs-8_x = callPackage ../development/web/nodejs-8_x/test.nix {};
-  rapture = callPackage ../tools/security/rapture/test.nix {};
-  vaulted = callPackage ../tools/security/vaulted/test.nix {};
-  xmlsec-openssl = callPackage ../development/libraries/xmlsec-openssl/test.nix {};
-}
+  # Packages that are darwin only for now
+  darwinPlatformPackages = {
+    graalvm11-ce-bin = callPackage ../development/compilers/graalvm/test-bin.nix { javaVersion = "11"; };
+    graalvm8-ce-bin = callPackage ../development/compilers/graalvm/test-bin.nix { javaVersion = "8"; };
+  };
+
+  crossPlatformPackages = {
+    hello = callPackage ./hello/test.nix {};
+    nodejs-8_x = callPackage ../development/web/nodejs-8_x/test.nix {};
+    rapture = callPackage ../tools/security/rapture/test.nix {};
+    vaulted = callPackage ../tools/security/vaulted/test.nix {};
+    xmlsec-openssl = callPackage ../development/libraries/xmlsec-openssl/test.nix {};
+  };
+
+  tests = {} // (if isDarwin then (crossPlatformPackages // darwinPlatformPackages) else crossPlatformPackages);
+
+in tests
