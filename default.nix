f4b96880d4681252a3019a0c4a92e4d021438070
Corban Raun <corban@raunco.co>
2020-05-17 15:12:07 -0600

    Install more packages when testing raunco channel
    vaulted is now an alias for vaulted-wrapped
    update vaulted wrapper

--- default.nix
+++ default.nix
@@ -12,13 +12,13 @@ let
     graalvm8-ce-bin = callPackage ./pkgs/development/compilers/graalvm/graalvm-ce-bin.nix { javaVersion = "8"; };
   };
 
-  crossPlatformPackages = {
+  crossPlatformPackages = rec {
     _nyx = callPackage ./pkgs/tools/system/nyx {}; # name conflics with another nix package hence _nyx
     dart = callPackage ./pkgs/development/interpreters/dart {};
     libpulsar = callPackage ./pkgs/development/libraries/libpulsar {};
     nodejs-8_x = callPackage ./pkgs/development/web/nodejs-8_x {};
     rapture = callPackage ./pkgs/tools/security/rapture {};
-    vaulted = callPackage ./pkgs/tools/security/vaulted {};
+    vaulted = vaulted-wrapped;
     vaulted-unwrapped = callPackage ./pkgs/tools/security/vaulted { enableWrapper = false; };
     vaulted-wrapped = callPackage ./pkgs/tools/security/vaulted { enableWrapper = true; };
     xmlsec-openssl = callPackage ./pkgs/development/libraries/xmlsec-openssl {};
--- pkgs/tools/security/vaulted/default.nix
+++ pkgs/tools/security/vaulted/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, buildGoModule, fetchFromGitHub, makeWrapper, enableWrapper ? true}:
+{ stdenv, buildGoModule, fetchFromGitHub, installShellFiles, makeWrapper, enableWrapper ? true}:
 
 buildGoModule rec {
   pname = "vaulted";
@@ -13,11 +13,20 @@ buildGoModule rec {
 
   modSha256 = "0rnxzv77fm3h0caaj7dgfbgy2z96ssn3vymp9bhbbdv54dqqjg1y";
 
-  # Since vaulted spawns a new shell /etc/profile gets called(at leas on OSX), which calls path_helper, which screws with the path
-  # unsetting __NIX_DARWIN_SET_ENVIRONMENT_DONE makes it so nix paths(such as run/current-system/sw/bin)  come before /bin /usr/bin etc.
-  nativeBuildInputs = stdenv.lib.optionals (enableWrapper && stdenv.isDarwin) [ makeWrapper ];
-  postInstall = ''''+ stdenv.lib.optionalString (enableWrapper && stdenv.isDarwin) ''
-    wrapProgram $out/bin/vaulted --set __NIX_DARWIN_SET_ENVIRONMENT_DONE ""
+  # Since vaulted spawns a new shell /etc/profile gets called(at least on OSX), which calls path_helper, which screws with the path
+  # unsetting some environment variables makes it so nix paths(such as run/current-system/sw/bin) come before /bin /usr/bin.
+  nativeBuildInputs = stdenv.lib.optionals enableWrapper [ installShellFiles makeWrapper ];
+  postInstall = ''
+    installManPage doc/man/*
+
+  ''+ stdenv.lib.optionalString enableWrapper ''
+    wrapProgram $out/bin/vaulted \
+      --set __ETC_BASHRC_SOURCED "" \
+      --set __ETC_ZPROFILE_SOURCED  "" \
+      --set __ETC_ZSHENV_SOURCED "" \
+      --set __ETC_ZSHRC_SOURCED "" \
+      ${stdenv.lib.optionalString (enableWrapper && stdenv.isDarwin)
+      ''--set __NIX_DARWIN_SET_ENVIRONMENT_DONE ""''}
   '';
 
   meta = with stdenv.lib; {
