30b62762a5697cdc845a4d7ae2e595250f423ef6
Corban Raun <corban@raunco.co>
2020-06-06 20:45:51 -0600

    Fix dart update

--- pkgs/development/interpreters/dart/default.nix
+++ pkgs/development/interpreters/dart/default.nix
@@ -2,7 +2,7 @@
 ,callPackage
 ,fetchurl
 ,unzip
-,version ? (if stdenv.isDarwin then "2.8.2" else "") }:
+,version ? (if stdenv.isDarwin then "2.8.4" else "") }:
 
 # Upstream nix appears to only support linux.
 # So, if were on osx, use the custom derivation, otherwise use upstream
